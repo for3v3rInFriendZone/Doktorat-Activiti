318e32bca035c14634d1d94b1a959ff2985be414
for3v3rInFriendZone <strile93@gmail.com>
2017-02-12 17:13:46 +0100

    Model has been updated and users.sql as well.

--- users.sql
+++ users.sql
@@ -1,66 +1,70 @@
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('nadRef', 2, 'Milos', 'Petrovic', 'nadRef@gmail.com', 'nadRef');
+    VALUES ('nadRef', 2, 'Milos', 'Petrovic', 'nadRef@gmail.com', 'test');
     
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('doktorant1', 2, 'Doktor1', 'Doktoric1', 'strile93@hotmail.com', 'doktorant1');
+    VALUES ('doktorant1', 2, 'Doktor1', 'Doktoric1', 'strile93@hotmail.com', 'test');
     
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('sefKatedre', 2, 'sefKatedre', 'sefKatedre', 'sefKat@gmail.com', 'sefKatedre');
+    VALUES ('sefKatedre', 2, 'Dragan', 'Dobric', 'sefKat@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('dekan', 2, 'Rade', 'Doroslovacki', 'radeDoros@gmail.com', 'dekan');
-    
-INSERT INTO act_id_user(
-            id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('mentor', 2, 'Moma', 'Mentor', 'strile93@hotmail.com', 'mentor');
+    VALUES ('dekanFTN', 2, 'Rade', 'Doroslovacki', 'radeDoros@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('ftnProfesor1', 2, 'Djordje', 'Dragic', 'djole@gmail.com', 'ftnProfesor1');
+    VALUES ('dekanPMF', 2, 'Milica', 'Pavkov', 'milicaPavkov@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('ftnProfesor2', 2, 'Petar', 'Misic', 'pera@gmail.com', 'ftnProfesor2');
+    VALUES ('dekanPravni', 2, 'Ljubomir', 'Stajic', 'ljubmirStajic@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('ftnProfesor3', 2, 'Ivan', 'Simic', 'ivan@gmail.com', 'ftnProfesor3');
+    VALUES ('ftnProfesor1', 2, 'Djordje', 'Dragic', 'djole@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('ftnProfesor4', 2, 'Nenad', 'Simovic', 'nenad@gmail.com', 'ftnProfesor4');
+    VALUES ('ftnProfesor2', 2, 'Petar', 'Misic', 'pera@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('ftnProfesor5', 2, 'Bosko', 'Dragic', 'bole@gmail.com', 'ftnProfesor5');
+    VALUES ('ftnProfesor3', 2, 'Ivan', 'Simic', 'ivan@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('ftnProfesor6', 2, 'Dragan', 'Petrovic', 'dragan@gmail.com', 'ftnProfesor6');
+    VALUES ('ftnProfesor4', 2, 'Nenad', 'Simovic', 'nenad@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('pmfProfesor1', 2, 'Zivan', 'Simic', 'zika@gmail.com', 'pmfProfesor1');
+    VALUES ('ftnProfesor5', 2, 'Bosko', 'Dragic', 'bole@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('pmfProfesor2', 2, 'Stefan', 'Obradovic', 'stefan@gmail.com', 'pmfProfesor2');
+    VALUES ('ftnProfesor6', 2, 'Dragan', 'Petrovic', 'dragan@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('etfProfesor1', 2, 'Denes', 'Nadj', 'denesn@gmail.com', 'etfProfesor1');
+    VALUES ('pmfProfesor1', 2, 'Zivan', 'Simic', 'zika@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('doktorant2', 2, 'Doktor2', 'Doktoric2', 'strile93@hotmail.com', 'doktorant2');
+    VALUES ('pmfProfesor2', 2, 'Stefan', 'Obradovic', 'stefan@gmail.com', 'test');
 
 INSERT INTO act_id_user(
             id_, rev_, first_, last_, email_, pwd_)
-    VALUES ('doktorant3', 2, 'Doktor3', 'Doktor3', 'strile93@hotmail.com', 'doktorant3');
+    VALUES ('pmfProfesor3', 2, 'Denes', 'Nadj', 'denesn@gmail.com', 'test');
+
+INSERT INTO act_id_user(
+            id_, rev_, first_, last_, email_, pwd_)
+    VALUES ('doktorant2', 2, 'Doktor2', 'Doktoric2', 'strile93@hotmail.com', 'test');
+
+INSERT INTO act_id_user(
+            id_, rev_, first_, last_, email_, pwd_)
+    VALUES ('doktorant3', 2, 'Doktor3', 'Doktor3', 'strile93@hotmail.com', 'test');
 
 ----------------------------------------------------------------------------------------------------
 
@@ -71,38 +75,56 @@ INSERT INTO act_id_user(
 INSERT INTO act_id_group(
             id_, rev_, name_)
     VALUES ('univerzitet', 1, 'Univerzitet');
+
 INSERT INTO act_id_group(
             id_, rev_, name_)
     VALUES ('nastNaucno', 1, 'Nastavno-naucno vece');
+
 INSERT INTO act_id_group(
             id_, rev_, name_)
     VALUES ('komisija_podobnost_teme', 1, 'Komisija za podobnost teme');
+
 INSERT INTO act_id_group(
             id_, rev_, name_)
     VALUES ('ftn', 1, 'FTN');
+
 INSERT INTO act_id_group(
             id_, rev_, name_)
     VALUES ('pmf', 1, 'PMF');
-INSERT INTO act_id_group(
-            id_, rev_, name_)
-    VALUES ('etf', 1, 'ETF');
+
 INSERT INTO act_id_group(
             id_, rev_, name_)
     VALUES ('studenti_doktorskih_studija', 1, 'Studenti doktorskih studija');
 
 INSERT INTO act_id_group(
             id_, rev_, name_)
-    VALUES ('profesoriFtn', 1, 'FTN profesori');
+    VALUES ('vece_studijskog_programa', 1, 'Vece studijskog programa');
+
+INSERT INTO act_id_group(
+            id_, rev_, name_)
+    VALUES ('profesoriFTN', 1, 'FTN profesori');
+
+INSERT INTO act_id_group(
+            id_, rev_, name_)
+    VALUES ('profesoriPMF', 1, 'PMF profesori');    
 
 ------------------------------------------------------------------------------------------------------
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('dekan', 'ftn');
+    VALUES ('dekanFTN', 'ftn');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('dekan', 'univerzitet');
+    VALUES ('dekanFTN', 'univerzitet');
+
+INSERT INTO public.act_id_membership(
+            user_id_, group_id_)
+    VALUES ('dekanPMF', 'univerzitet');
+
+INSERT INTO public.act_id_membership(
+            user_id_, group_id_)
+    VALUES ('dekanPravni', 'univerzitet');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
@@ -122,10 +144,6 @@ INSERT INTO public.act_id_membership(
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor1', 'univerzitet');
-
-INSERT INTO public.act_id_membership(
-            user_id_, group_id_)
     VALUES ('ftnProfesor1', 'nastNaucno');
 
 INSERT INTO public.act_id_membership(
@@ -134,23 +152,15 @@ INSERT INTO public.act_id_membership(
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor2', 'univerzitet');
-
-INSERT INTO public.act_id_membership(
-            user_id_, group_id_)
     VALUES ('ftnProfesor2', 'nastNaucno');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('mentor', 'univerzitet');
+    VALUES ('ftnProfesor4', 'nastNaucno');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('mentor', 'ftn');
-
-INSERT INTO public.act_id_membership(
-            user_id_, group_id_)
-    VALUES ('pmfProfesor1', 'univerzitet');
+    VALUES ('dekanPMF', 'pmf');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
@@ -158,15 +168,11 @@ INSERT INTO public.act_id_membership(
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('pmfProfesor2', 'univerzitet');
-
-INSERT INTO public.act_id_membership(
-            user_id_, group_id_)
     VALUES ('pmfProfesor2', 'pmf');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('etfProfesor1', 'etf');
+    VALUES ('pmfProfesor3', 'pmf');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
@@ -182,28 +188,47 @@ INSERT INTO public.act_id_membership(
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor1', 'profesoriFtn');
+    VALUES ('ftnProfesor1', 'profesoriFTN');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor2', 'profesoriFtn');
+    VALUES ('ftnProfesor2', 'profesoriFTN');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor3', 'profesoriFtn');
+    VALUES ('ftnProfesor3', 'profesoriFTN');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor4', 'profesoriFtn');
+    VALUES ('ftnProfesor4', 'profesoriFTN');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor5', 'profesoriFtn');
+    VALUES ('ftnProfesor5', 'profesoriFTN');
 
 INSERT INTO public.act_id_membership(
             user_id_, group_id_)
-    VALUES ('ftnProfesor6', 'profesoriFtn');
+    VALUES ('ftnProfesor6', 'profesoriFTN');
 
+INSERT INTO public.act_id_membership(
+            user_id_, group_id_)
+    VALUES ('pmfProfesor1', 'profesoriPMF');
+
+INSERT INTO public.act_id_membership(
+            user_id_, group_id_)
+    VALUES ('pmfProfesor2', 'profesoriPMF');
+
+INSERT INTO public.act_id_membership(
+            user_id_, group_id_)
+    VALUES ('pmfProfesor3', 'profesoriPMF');
+
+INSERT INTO public.act_id_membership(
+            user_id_, group_id_)
+    VALUES ('ftnProfesor3', 'vece_studijskog_programa');
+
+INSERT INTO public.act_id_membership(
+            user_id_, group_id_)
+    VALUES ('ftnProfesor5', 'vece_studijskog_programa');
 
 
 
